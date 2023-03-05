240e36ee9d7e9ebd3517bd488705512dbb02c13a
Joshua J Piluden <joshuajpiluden@Gmail.com>
2023-03-05 10:56:02 +0800

    Added  commands.

---
+++ SM-POST/Post.Cmd/Post.Cmd.Api/Commands/AddCommentCommand.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class AddCommentCommand : BaseCommand
+    {
+        public string Comment { get; set; } = string.Empty; 
+        public string Message { get; set; } = string.Empty;
+    }
+}
